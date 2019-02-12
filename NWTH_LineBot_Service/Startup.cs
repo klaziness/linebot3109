5a367f3b6a9192bc86fea866404aeb087ebad4c4
Nuttapong Chaipanya (Aek) <nch@naviworldth.com>
2019-02-12 21:46:27 +0700

    Add project files.

---
+++ NWTH_LineBot_Service/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(NWTH_LineBot_Service.Startup))]
+
+namespace NWTH_LineBot_Service
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Wire Web API 
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.MapHttpAttributeRoutes();
+
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
